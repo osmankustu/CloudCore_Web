b8f6983cf59a373dce271a56a5d29fa658574f7a
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2025-08-13 11:53:06 +0300

    [FEATURE] Service feature lint and dom error Fixes #16

--- src/app/(admin)/management/document-viewer/page.tsx
+++ src/app/(admin)/management/document-viewer/page.tsx
@@ -4,14 +4,18 @@ import React, { Suspense } from "react";
 
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import Spinner from "@/components/ui/spinner/Spinner";
-import DocumentViewer from "@/features/document/components/cards/documentViewer";
+import dynamic from "next/dynamic";
+const DocumentViewer = dynamic(
+  () => import("@/features/document/components/cards/documentViewer"),
+  { ssr: false }, // <--- SSR kapalı
+);
 
 const page = () => {
   return (
     <div>
       <PageBreadcrumb pageTitle={"Döküman Görüntüleyici"} />
       <div>
-        <Suspense fallback={<Spinner />}>{<DocumentViewer />}</Suspense>
+        <DocumentViewer />
       </div>
     </div>
   );
