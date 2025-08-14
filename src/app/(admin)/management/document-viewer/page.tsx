c39b3f7f984485a1d019573e5883c4237a49bd47
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2025-08-14 22:18:17 +0300

    [FEATURE] Centralized logger Suspense error Fixes #10

--- src/app/(admin)/management/document-viewer/page.tsx
+++ src/app/(admin)/management/document-viewer/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import Spinner from "@/components/ui/spinner/Spinner";
 import DocumentViewer from "@/features/document/components/cards/documentViewer";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <div>
       <PageBreadcrumb pageTitle={"Döküman Görüntüleyici"} />
       <div>
-        <DocumentViewer />
+        <Suspense fallback={<Spinner />}>
+          <DocumentViewer />
+        </Suspense>
       </div>
     </div>
   );
