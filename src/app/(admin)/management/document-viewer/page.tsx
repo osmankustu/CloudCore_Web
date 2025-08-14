8efc4b8e7299b8139527c85505a90f569753cc40
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2025-08-14 20:47:11 +0300

    [FEATURE] Centralized logger Fixes #10

--- src/app/(admin)/management/document-viewer/page.tsx
+++ src/app/(admin)/management/document-viewer/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import React, { Suspense } from "react";
-
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import Spinner from "@/components/ui/spinner/Spinner";
 import dynamic from "next/dynamic";
 const DocumentViewer = dynamic(
   () => import("@/features/document/components/cards/documentViewer"),
@@ -15,9 +12,7 @@ const page = () => {
     <div>
       <PageBreadcrumb pageTitle={"Döküman Görüntüleyici"} />
       <div>
-        <Suspense fallback={<Spinner />}>
-          <DocumentViewer />
-        </Suspense>
+        <DocumentViewer />
       </div>
     </div>
   );
